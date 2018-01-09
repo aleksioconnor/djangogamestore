4e81e7b150cddcb6ffa13bc68cd016ca9fdff50c
aleksioconnor <aleksi.oconnor@gmail.com>
2018-01-09 14:23:04 +0200

    Initial commit

---
+++ wsd2017gamestore/wsd2017gamestore/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wsd2017gamestore project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wsd2017gamestore.settings")
+
+application = get_wsgi_application()
