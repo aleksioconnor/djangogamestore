8e850dc0ed97d6fd767f507310f0196266e9eeec
aleksioconnor <aleksi.oconnor@gmail.com>
2018-01-11 12:07:00 +0200

    Added new hosts to settings

--- wsd2017gamestore/wsd2017gamestore/settings.py
+++ wsd2017gamestore/wsd2017gamestore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'u8%crus(o8$-!=09d_so+gs)ces42mv&m(=hsonzv7w@6$xph2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['wsd2017gamestore.herokuapp.com']
+ALLOWED_HOSTS = ['wsd2017gamestore.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
