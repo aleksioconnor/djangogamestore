7b1f0ba63d4b152ebcf7bed11986ecef5d01318c
aleksioconnor <aleksi.oconnor@gmail.com>
2018-01-11 13:17:18 +0200

    Fixed typo

--- wsd2017gamestore/wsd2017gamestore/settings.py
+++ wsd2017gamestore/wsd2017gamestore/settings.py
@@ -20,7 +20,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'
+    os.path.join(BASE_DIR, 'static')
 )
 
 # Quick-start development settings - unsuitable for production
