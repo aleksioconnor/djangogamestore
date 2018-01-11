4f9a347d6b552b4407d01408e9a5186c86bf2557
aleksioconnor <aleksi.oconnor@gmail.com>
2018-01-11 13:13:02 +0200

    Heroku config

--- wsd2017gamestore/wsd2017gamestore/settings.py
+++ wsd2017gamestore/wsd2017gamestore/settings.py
@@ -16,6 +16,12 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
