0f5257d488e83b9291536e59946bd07d7fc68660
Alan Pirdil <alan.pirdil@aalto.fi>
2018-01-16 14:58:20 +0200

    Additional form fields

--- wsd2017gamestore/registration/views.py
+++ wsd2017gamestore/registration/views.py
@@ -1,19 +1,21 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user_type = form.cleaned_data.get('user_type')
+            user = authenticate(username=username, password=raw_password, user_type=user_type)
             login(request, user)
             return redirect('/hello/')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
 
         #TODO: allaoleva form:form contextina eteenpäin
     return render(request, 'registration/index.html', {'form': form})
