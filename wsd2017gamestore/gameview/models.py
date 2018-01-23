95e8fb4018ae9c39f76292536db73a70a9d88526
aleksioconnor <aleksi.oconnor@gmail.com>
2018-01-23 18:14:53 +0200

    Added highScore model and implemented rudimental high score sorting

--- wsd2017gamestore/gameview/models.py
+++ wsd2017gamestore/gameview/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 from store.models import Game
 
-# Create your models here.
+# Highscore model to save the high scores of a single game
 class HighScore(models.Model):
-    player = models.ForeignKey(User)
-    score = models.IntegerField()
-    date = models.DateTimeField(auto_now_add=True)
-    game = models.ForeignKey(Game)
+	player = models.ForeignKey(User)
+	score = models.IntegerField()
+	date = models.DateTimeField(default=timezone.now)
+	game = models.ForeignKey(Game)
+
+	# string representation of this object for example when logged to console
+	# will only contain its score (eg. makes it more human friendly when debugging)
+	def __str__(self):
+		return str(self.score)
--- wsd2017gamestore/gameview/views.py
+++ wsd2017gamestore/gameview/views.py
@@ -3,15 +3,30 @@ from django.http import HttpResponse
 from django.template import loader
 
 from store.models import Game
-# Create your views here.
+from .models import HighScore
 
 def index(request, game_id):
 	current_game = Game.objects.get(id=game_id)
 	context = {'game': current_game}
 	return render(request, 'gameview/index.html', context)
 
+# Defines an 'endpoint' for our ajax POST function in the gameview template.
+# When submitting a score, a function sends the score with an ajax function
+# and it gets handled here.
 def score(request, game_id):
+	# Request contains the crsftoken and the data sent from the template
+	# can be accessed via POST
 	post = request.POST
+	# Accessed via the key value
 	score = post['score']
-	print(result)
-	return HttpResponse("jee")
+	# gets the correct game being played
+	currentGame = Game.objects.get(id=game_id)
+	# adds a new high score model/object
+	newScore = HighScore(player=request.user, score=score, game=currentGame)
+	newScore.save()
+
+	# Retrieves all high score objects, filters out only the ones
+	# associated with this game, orders them, and only returns
+	# the top 5.
+	scores = HighScore.objects.all().filter(game=currentGame).order_by('-score')[:5]
+	return HttpResponse(scores)
