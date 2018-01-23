0c689d178d09b15d47115372eb2498351e8c7e8b
Katri Saarinen <katri@dhcp-84-207.eduroam.aalto.fi>
2018-01-23 17:00:35 +0200

    getting high score from game to views

--- wsd2017gamestore/gameview/models.py
+++ wsd2017gamestore/gameview/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from store.models import Game
 
 # Create your models here.
+class HighScore(models.Model):
+    player = models.ForeignKey(User)
+    score = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    game = models.ForeignKey(Game)
--- wsd2017gamestore/gameview/urls.py
+++ wsd2017gamestore/gameview/urls.py
@@ -4,4 +4,5 @@ from . import views
 # The url mapping takes a four digit number that should be mapped to an individual game.
 urlpatterns = [
 	url(r'^(?P<game_id>[0-9]+)/$', views.index, name='index'),
+	url(r'^(?P<game_id>[0-9]+)/score/', views.score),
 ]
