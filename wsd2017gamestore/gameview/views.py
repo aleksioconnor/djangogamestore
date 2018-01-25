010fc20d0e7483fd96227bb0385e3da1a66e9bc9
aleksioconnor <aleksi.oconnor@gmail.com>
2018-01-25 17:22:27 +0200

    High score now prints out in gameview

--- wsd2017gamestore/gameview/views.py
+++ wsd2017gamestore/gameview/views.py
@@ -5,6 +5,11 @@ from django.template import loader
 from store.models import Game
 from .models import HighScore
 
+from django.http import JsonResponse
+from django.core import serializers
+
+import json
+
 def index(request, game_id):
 	current_game = Game.objects.get(id=game_id)
 	context = {'game': current_game}
@@ -13,6 +18,11 @@ def index(request, game_id):
 # Defines an 'endpoint' for our ajax POST function in the gameview template.
 # When submitting a score, a function sends the score with an ajax function
 # and it gets handled here.
+
+#TODO & IMPORTANT: Not sure, but i'm pretty confident that the score input can be manipulated. For example,
+# if the score key-value is intercepted a mysql-injection can be performed. This is something that should
+# be investigated
+
 def score(request, game_id):
 	# Request contains the crsftoken and the data sent from the template
 	# can be accessed via POST
@@ -29,4 +39,6 @@ def score(request, game_id):
 	# associated with this game, orders them, and only returns
 	# the top 5.
 	scores = HighScore.objects.all().filter(game=currentGame).order_by('-score')[:5]
-	return HttpResponse(scores)
+
+	# Safe is false to allow non-dict objects to be serialized
+	return JsonResponse(serializers.serialize('json', scores), safe=False)
