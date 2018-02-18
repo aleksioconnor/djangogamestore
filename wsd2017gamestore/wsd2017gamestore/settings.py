587c2efed06419341b82b289ae213681c41dffda
aleksioconnor <aleksi.oconnor@gmail.com>
2018-02-18 15:08:53 +0200

    Changed staticfiles config

--- wsd2017gamestore/wsd2017gamestore/settings.py
+++ wsd2017gamestore/wsd2017gamestore/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
