925496cc5a6999c529d3f84c08394b50a3740911
aleksioconnor <aleksi.oconnor@gmail.com>
2018-02-18 15:18:46 +0200

    Changed staticfiles dir

--- wsd2017gamestore/wsd2017gamestore/settings.py
+++ wsd2017gamestore/wsd2017gamestore/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
