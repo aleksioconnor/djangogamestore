288168ad8fadadab0e51f5a3e91dd8fbbdd5d90b
aleksioconnor <aleksi.oconnor@gmail.com>
2018-02-18 20:42:04 +0200

    Added gzipping to staticfiles

--- wsd2017gamestore/wsd2017gamestore/settings.py
+++ wsd2017gamestore/wsd2017gamestore/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # url where staticfiles are served from
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # where static gatherer looks for static files
 STATICFILES_DIRS = (
