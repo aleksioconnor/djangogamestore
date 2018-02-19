adbfab90ff41a7f6d7dabb760e96c3e62b88c375
Katri Saarinen <katri@dhcp-85-110.eduroam.aalto.fi>
2018-02-19 19:26:07 +0200

    category name must be unique

--- wsd2017gamestore/store/models.py
+++ wsd2017gamestore/store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     def __str__(self):
       return self.name
 
