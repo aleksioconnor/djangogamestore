8ae5ad58666b1b9208826d4c0a84f6366dbc1018
aleksioconnor <aleksi.oconnor@gmail.com>
2018-02-19 21:39:23 +0200

    debug false and login redirect

--- wsd2017gamestore/authentication/urls.py
+++ wsd2017gamestore/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from .views import login_view
 
 # At the moment no other implementation than url mappings to auth_views.login and auth_views.logout
 urlpatterns = [
-	url('login/', auth_views.login, {'template_name': 'login/login.html'}, name='login'),
+	url('login/', login_view, name='login'),
 	url('logout/', auth_views.logout, {'next_page': '/'}),
 ]
